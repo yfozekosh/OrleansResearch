6dcae8cf5d4808e1e96c7d7706d15503c84f8515
yurii.fozekosh <yurii.fozekosh@eleks.com>
2018-08-23 12:27:26 +0300

    implement simple clustering

--- Orle3/Program.cs
+++ Orle3/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using GrainContracts;
+using GrainImpl;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
 
 namespace Orle3
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("Press Enter to terminate...");
+                Console.ReadLine();
+
+                await host.StopAsync();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey();
+                return 1;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            Console.Write("Port\r\n>>>");
+
+
+            int p = int.Parse(Console.ReadLine());
+
+            var builder = new SiloHostBuilder()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "HelloWorldApp";
+                })
+                .UseAdoNetClustering(options =>
+                {
+                    options.Invariant = "System.Data.SqlClient";
+                    options.ConnectionString =
+                        "Data Source=localhost,1434;Initial Catalog=OrleDb;Persist Security Info=True;User ID=sa;Password=123456";
+                })
+                .ConfigureEndpoints(siloPort: p, gatewayPort: 30000)
+                //.ConfigureApplicationParts(parts =>
+                //    parts.AddApplicationPart(typeof(IHello).Assembly).WithReferences())
+                .UseDashboard(options => { options.Port = 8090; })
+                .ConfigureLogging(loggingBuilder => loggingBuilder.SetMinimumLevel(LogLevel.Warning).AddConsole());
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
         }
     }
 }
---
+++ OrleClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using GrainContracts;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+
+namespace OrleClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            IClientBuilder clientBuilder = new Orleans.ClientBuilder()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "HelloWorldApp";
+                })
+                .UseAdoNetClustering(options =>
+                {
+                    options.Invariant = "System.Data.SqlClient";
+                    options.ConnectionString =
+                        "Data Source=localhost,1434;Initial Catalog=OrleDb;Persist Security Info=True;User ID=sa;Password=123456";
+                })
+                .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Warning).AddConsole());
+
+            using (IClusterClient client = clientBuilder.Build())
+            {
+                await Task.Delay(TimeSpan.FromSeconds(10));
+
+                await client.Connect();
+                while (true)
+                {
+                    Console.Write("please write ID\r\n>>>");
+                    int id = int.Parse(Console.ReadLine());
+                    if (id==-1) break;
+                    
+                    IHello hello = client.GetGrain<IHello>(id);
+
+                    var res = await hello.SayHello("hohoh");
+
+                    Console.WriteLine($"Received: {res}");
+                }
+            }
+        }
+    }
+}
